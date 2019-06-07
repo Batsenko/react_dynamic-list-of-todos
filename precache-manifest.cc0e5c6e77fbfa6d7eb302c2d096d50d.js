4345e2fecfd87248b4f97c8a65a6539550c52ba9
Dmitry Batsenko <batsenkod@gmail.com>
2019-06-07 16:12:46 +0300

    Updates

---
+++ precache-manifest.cc0e5c6e77fbfa6d7eb302c2d096d50d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11da6a7072a9b3e0716e854b9053309f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c5deebe5ca22d47aa7b",
+    "url": "./static/css/main.02167290.chunk.css"
+  },
+  {
+    "revision": "86fec8d389072113a12c",
+    "url": "./static/js/2.b025d17d.chunk.js"
+  },
+  {
+    "revision": "3c5deebe5ca22d47aa7b",
+    "url": "./static/js/main.7a30f01e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
